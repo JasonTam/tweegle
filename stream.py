411421496808e62f83d7f0a058757f517af74ed3
Eric Leong <leong@cooper.edu>
2014-04-12 17:08:33 -0400

    Select information from tweet metadata.

--- stream.py
+++ stream.py
@@ -20,7 +20,12 @@ class StreamListener(tweepy.StreamListener):
         if data:
             tweet = json.loads(data)
 
-            self.tweets.append(tweet)
+            info = {}
+            info['user'] = tweet['user']
+            info['coordinates'] = tweet['coordinates']
+            info['text'] = tweet['text']
+
+            self.tweets.append(info)
 
             if len(self.tweets) < self.limit:
 
