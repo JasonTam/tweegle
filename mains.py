d550e772b88a9c47d2cc02d1fce4725fb7f76fcb
Eric Leong <leong@cooper.edu>
2014-04-12 21:52:13 -0400

    Add main check.

--- mains.py
+++ mains.py
@@ -18,43 +18,39 @@ import itertools
 import preprocess
 import features
 
+if __name__ == "__main__":
 
+    import sys
 
-# Load data into Python Dict
-dir_data = './data'
-filename = 'test.json'
-filepath = os.path.join(dir_data, filename)
-with open(filepath, 'r') as f:
-    tweets = json.load(f)
+    # Load data into Python Dict
+    filename = sys.argv[1] if len(sys.argv) > 1 else 'data/data.json'
 
-# Preprocess data from json into data structs
-docs_terms, docs_t_collection = preprocess.setup_doc_collection(tweets)
+    with open(filename, 'r') as f:
+        tweets = json.load(f)
 
-# Getting location info
-targets = preprocess.get_target_map(tweets)
-loc_to_id = defaultdict(set)
-for k, v in targets.iteritems():
-    loc_to_id[v].add(k)
-all_locations = set(loc_to_id.keys())
+        # Preprocess data from json into data structs
+        docs_terms, docs_t_collection = preprocess.setup_doc_collection(tweets)
 
-# Combine tweets from same location
-mega_docs = {loc:
-             list(itertools.chain(
-                 *[docs_terms[id].tokens for id in loc_to_id[loc]]
-             ))
-             for loc in all_locations
-            }
+        # Getting location info
+        targets = preprocess.get_target_map(tweets)
+        loc_to_id = defaultdict(set)
+        for k, v in targets.iteritems():
+            loc_to_id[v].add(k)
+        all_locations = set(loc_to_id.keys())
 
-tfidf = TfidfVectorizer(stop_words='english')
+        # Combine tweets from same location
+        mega_docs = {loc:
+                     list(itertools.chain(
+                         *[docs_terms[id].tokens for id in loc_to_id[loc]]
+                     ))
+                     for loc in all_locations
+                    }
 
-# Get feature vector for a given location as the mean of tfidf
-tfs = tfidf.fit_transform(docs_t_collection.tokens)
-loc_feats = {}
-for loc in all_locations:
-    loc_feats[loc] = tfidf.transform(mega_docs[loc]).mean(0)
+        tfidf = TfidfVectorizer(stop_words='english')
 
-
-
-
-
-
+        # Get feature vector for a given location as the mean of tfidf
+        tfs = tfidf.fit_transform(docs_t_collection.tokens)
+        loc_feats = {}
+        
+        for loc in all_locations:
+            loc_feats[loc] = tfidf.transform(mega_docs[loc]).mean(0)
